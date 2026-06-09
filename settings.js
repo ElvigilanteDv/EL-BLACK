3c28efe6828fd9a9ee0cee9cc6ef70837ea6e5d5
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-06-08 21:47:33 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+global.owner = ['5492644138998','584125877491']
+global.botNumber = ''
+
+global.sessionName = 'Sessions/Owner'
+global.version = '🐉🌀 3.0 - Gotenks V1 🌀🐉'
+global.dev = "⚡ Powered by Gotenks V1 Bot ⚡"
+global.wm = "Gotenks-V1 🐉"
+global.links = {
+api: 'Gotenks API 🌀',
+channel: "https://whatsapp.com/channel/0029Vb7ntULLY6d8uOvyDy0C",
+github: "https://github.com/dvwilker",
+gmail: "developer.wilker.ofc@gmail.com"
+}
+global.my = {
+ch: '120363404707199986@newsletter',
+name: '🐉✦ Gotenks V1 Bot ✦🌀',
+}
+
+global.mess = {
+socket: '🐉🌀 Este comando solo puede ser ejecutado por un Socket Gotenks.',
+admin: '⚡ Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+botAdmin: '🌀 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo Gotenks.'
+}
+
+global.APIs = {
+adonix: { url: "https://api-adonix.ultraplus.click", key: "Danielrxz" },
+vreden: { url: "https://api.vreden.web.id", key: null },
+nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+stellar: { url: "https://api.stellarwa.xyz", key: wm },
+apifaa: { url: "https://api-faa.my.id", key: null },
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null }
+}
